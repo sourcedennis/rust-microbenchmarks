ce6ff22d255a4173487020145b0cefc83e50be2c
Dennis <me@dennis.life>
2022-10-27 14:40:33 +0200

    removed unnecessary function call

--- parse-u64s/src/main.rs
+++ parse-u64s/src/main.rs
@@ -1,12 +1,12 @@
 
 // stdlib imports
-use std::{fmt::Write, time::SystemTime};
 use std::fmt;
+use std::{fmt::Write, time::SystemTime};
 use nom::multi::many0;
-use nom::sequence::{terminated};
+use nom::sequence::terminated;
+use nom::character::complete::u64;
 // external library imports
 use rand::{Rng, thread_rng};
-use nom::{IResult, Parser};
 use nom::error::{Error};
 use nom::bytes::streaming::tag;
 
@@ -17,13 +17,13 @@ use nom::bytes::streaming::tag;
 /// * [`nom`] (with [`nom::character::complete::u64`])
 pub fn main( ) {
   let n = 10_000_000;
-  let str = gen_str( n ).unwrap( );
-  println!( "Generated string of size: {}", str.len( ) );
+  let input = gen_str( n ).unwrap( );
+  println!( "Generated string of size: {}", input.len( ) );
 
   // Benchmark .split()
   let now = SystemTime::now( );
   for _i in 0..10 {
-    let lines = str.split( "\n" );
+    let lines = input.split( "\n" );
     let parsed: Vec< u64 > = lines.into_iter( ).filter_map( |x| x.parse::< u64 >( ).ok( ) ).collect( );
     debug_assert!( parsed.len( ) == n );
   }
@@ -32,7 +32,9 @@ pub fn main( ) {
   // Benchmark nom
   let now = SystemTime::now( );
   for _i in 0..10 {
-    let (rem_input, parsed) = many0( terminated( nom::character::complete::u64, p_discard( tag( "\n" ) ) ) )( &str ).unwrap( );
+    // Without the type annotation (of `u64()`), the error infers to:
+    // `Error< &String >`, which fails.
+    let (rem_input, parsed) = many0( terminated( u64::< _, Error< &str > >, tag( "\n" ) ) )( &input ).unwrap( );
     debug_assert!( rem_input == "" );
     debug_assert!( parsed.len( ) == n );
   }
@@ -48,17 +50,4 @@ fn gen_str( n: usize ) -> Result< String, fmt::Error > {
     out.write_fmt( format_args!( "{}\n", rng.gen::< u64 >( ) ) )?;
   }
   Ok( out )
-}
-
-/// Discard the parser's output.
-fn p_discard< 'a, A, F >(
-  mut p: F
-) -> impl FnMut( &'a str ) -> IResult< &'a str, () >
-  where
-    F : Parser< &'a str, A, Error< &'a str > >
-{
-  move |input| {
-    let (input, _) = p.parse( input )?;
-    Ok( (input, ()) )
-  }
 }
